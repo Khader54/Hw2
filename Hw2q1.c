7637a9bab837799dd4a3be49dbff7bd963bc750c
Khader <abdkeb2@gmail.com>
2024-12-08 00:14:29 +0200

    IDK

---
+++ Hw2q1.c
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define ARR_MAX_LENGTH 50
+
+
+int main()
+{
+    int seen[ARR_MAX_LENGTH] = {0};
+
+    int number, copyNumber, sumOfSquares = 0;
+
+    printf("please enter a number:\n");
+    scanf("%d", &number);
+    copyNumber = number;
+
+
+    for (int i = 0; i < ARR_MAX_LENGTH; i++)
+    {
+        while (copyNumber != 0)
+        {
+            sumOfSquares = (sumOfSquares + ((copyNumber%10) * (copyNumber%10)));
+            copyNumber = (copyNumber/10);
+        }
+        if(sumOfSquares == 1)
+        {
+            printf("the number %d is happy\n", number);
+            return 0;
+        }
+        copyNumber = sumOfSquares;
+        seen[i] = sumOfSquares;
+        sumOfSquares = 0;
+        //printf("%d\n", seen[i]);
+
+        for (int j = i - 1; j > 0; j--)
+            if(seen[i] == seen[j] && seen[i] != 1)
+            {
+                printf("the number %d is not happy\n", number);
+                return 0;
+            }
+    }
+    printf("the number %d is not happy\n", number);
+    return 0;
+}

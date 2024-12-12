82bd7b13f3426230fcac88d99ed96ec5dffc683e
Khader54 <abdkeb2@gmail.com>
2024-12-12 13:40:30 +0200

    Add files via upload

---
+++ hw2q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define ARR_MAX_LENGTH 50
+
+//"please enter a number:\n"
+//"the number %d is happy"
+//"the number %d is not happy"
+
+
+int main() {
+    int seen[ARR_MAX_LENGTH] = {0};
+
+    return 0;
+}
